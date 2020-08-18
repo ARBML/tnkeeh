4d8808216e2bd316ea0a11dcafef6d021819f993
zaidalyafeai <alyafey22@gmail.com>
2020-08-18 03:02:54 +0300

    change version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as readme_file:
     readme = readme_file.read()
 
 setup(name='tnkeeh',
-      version='0.0.1',
+      version='0.0.2',
       url='',
       discription="Arabic Preprocessing Library",
       long_description=readme,
